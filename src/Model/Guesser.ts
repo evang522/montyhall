7d0f5207743408314091e7140479af79be156d55
Evan Garrett <evan@fitfox.de>
2019-09-06 22:55:06 +0200

    Correct Documentation and remove unused files

--- src/Model/Guesser.ts
+++ src/Model/Guesser.ts
@@ -19,7 +19,7 @@ class Guesser {
     {
         // now that you have made a guess, another of the doors needs to be opened to reveal that it is not the correct answer
         // so for example if you chose index 1, then index 3 would have to be revealed as the next incorrect answer. 
-        // Then, if a switch guesser, switch.
+        // Then, if switchAnswers is true, then switch.
         if (!this.switchAnswers)
         {
             this.finalAnswer = this.initialGuess;
